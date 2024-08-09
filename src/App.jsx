cc1b7588d792f9cba8f73d8334e491dad25c9f2a
OsMax <www.osmax@gmail.com>
2024-08-09 14:32:32 +0300

    route

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import logo from "./logo.svg";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import HomePage from "./pages/HomePage/HomePage";
 import "./App.css";
 
 function App() {
-  return <div>TEST</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        {/* <Route path="catalog" element={<CarsPage />} />
+        <Route path="favorite" element={<FavoritePage />} /> */}
+      </Route>
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
 }
 
 export default App;
