04b0f83532126c1cc6a10b054d8dc54d6e3b32d1
OsMax <www.osmax@gmail.com>
2024-08-13 15:59:04 +0300

    redux store

---
+++ src/redux/store.js
@@ -0,0 +1,38 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import { authReducer } from "./auth/authSlice";
+import { recipeReducer } from "./recipe/recipeSlice";
+
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+  whitelist: ["token"],
+};
+
+const persistRed = persistReducer(persistConfig, authReducer);
+
+export const store = configureStore({
+  reducer: {
+    auth: persistRed,
+    recipes: recipeReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
