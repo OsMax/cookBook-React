ee8ba0c6cba941f3bb2b22f64a3fd897d173d184
OsMax <www.osmax@gmail.com>
2024-08-15 13:09:09 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="cookBook">
+    <BrowserRouter basename="cookbook-react">
       <App />
     </BrowserRouter>
   </React.StrictMode>
